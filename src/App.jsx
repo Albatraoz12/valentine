6e4b9cb38dd4461948d6e75c277f32ec4376f03a
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-02-05 10:42:30 +0100

    random commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <section>
+      <h1>Will you be my valentine?</h1>
+      <img
+        src='../public/images/Cute-Cat.jpg'
+        alt='cute cat'
+        className='cat-image'
+      />
+      <div className='buttons-container'>
+        <button type='button' className='btn btn-enhance'>
+          Yes ofc
+        </button>
+        <button
+          type='button'
+          className='btn'
+          onClick={() => setCount(count + 1)}
+        >
+          No
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
