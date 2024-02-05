b88bdfc259ed3872be7239cf9b350c0f6aa8da26
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-02-05 11:02:54 +0100

    fic img scr and add functionallity to button if pressed no

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,36 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [valentine, setValentine] = useState(false);
 
   return (
     <section>
+      {count}
+      <br />
+      {valentine.toString()}
       <h1>Will you be my valentine?</h1>
-      <img
-        src='../public/images/Cat_heart.jpg'
-        alt='cute cat'
-        className='cat-image'
-      />
+      <img src='/images/Cat_heart.jpg' alt='cute cat' className='cat-image' />
       <div className='buttons-container'>
-        <button type='button' className='btn btn-enhance'>
+        <button
+          type='button'
+          className='btn btn-enhance'
+          style={
+            count > 0 ? { height: `${count}rem`, width: `${count}rem` } : {}
+          }
+          onClick={() => {
+            setValentine(true);
+            setCount(0);
+          }}
+        >
           Yes ofc
         </button>
         <button
           type='button'
           className='btn'
-          onClick={() => setCount(count + 1)}
+          onClick={() => {
+            setCount(count + 5);
+            setValentine(false);
+          }}
         >
           No
         </button>
